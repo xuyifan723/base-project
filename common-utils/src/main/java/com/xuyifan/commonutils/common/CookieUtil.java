371a9159a041fef158a7aa439340423d89e4e165
xuyifan <1135672070@qq.com>
2019-09-03 11:56:19 +0800

    +++++++

---
+++ common-utils/src/main/java/com/xuyifan/commonutils/common/CookieUtil.java
@@ -0,0 +1,90 @@
+package com.xuyifan.commonutils.common;
+
+
+import org.apache.commons.lang.StringUtils;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Classname CookieUtil
+ * @Description TODO
+ * @Date 2019/7/8 0008 上午 10:31
+ * @Created by XUYIFAN723
+ */
+public class CookieUtil {
+    //存用户的cookie名称
+    public static final String USER_COOKIE_NAME = "Data_User";
+    //用户存的过期时间为30分钟
+    public static final Long EXPIRE_TIME = 1000 * 60 * 30L;
+
+    /**
+     * 从cookie里面拿出具体cookie的值
+     *
+     * @param name
+     * @param request
+     * @return
+     */
+    public static String getCookie(String name, HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String userValue = "";
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(name)) {
+                userValue = AESUtil.decrypt(cookie.getValue());
+                break;
+            }
+        }
+        return userValue;
+    }
+
+    /**
+     * 从cookie里面拿出用户信息
+     *
+     * @param request
+     * @return
+     */
+    public static String getUser(HttpServletRequest request) {
+        String userValue = getCookie(USER_COOKIE_NAME, request);
+        String userName = "";
+        if (!StringUtils.isEmpty(userValue)) {
+            String[] userInfo = userValue.split("_");
+            userName = userInfo[0];
+            Long loginTime = Long.valueOf(userInfo[1]);
+            boolean flag = System.currentTimeMillis() - EXPIRE_TIME > loginTime;
+            if (!flag) {
+                throw new RuntimeException("user is expire,please login");
+            }
+        }
+        return userName;
+    }
+
+    /**
+     * 验证用户信息是否过期
+     *
+     * @param request
+     * @return
+     */
+    public static boolean validateUser(HttpServletRequest request) {
+        String userValue = getCookie(USER_COOKIE_NAME, request);
+        boolean flag = false;
+        if (!StringUtils.isEmpty(userValue)) {
+            String[] userInfo = userValue.split("__");
+            Long loginTime = Long.valueOf(userInfo[1]);
+            flag = System.currentTimeMillis() - EXPIRE_TIME > loginTime;
+        }
+        return flag;
+    }
+
+    /**
+     * 往cookie里面添加用户信息
+     *
+     * @param response
+     * @param username
+     */
+    public static void addUser(HttpServletResponse response, String username) {
+        String userInfo = username + "__" + System.currentTimeMillis();
+        Cookie cookie = new Cookie(USER_COOKIE_NAME, userInfo);
+        response.addCookie(cookie);
+    }
+
+}
---
+++ common-utils/src/main/java/com/xuyifan/commonutils/common/StrUtil.java
@@ -0,0 +1,38 @@
+package com.xuyifan.commonutils.common;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @Classname StrUtil
+ * @Description TODO
+ * @Date 2019/7/8 0008 下午 1:25
+ * @Created by XUYIFAN723
+ */
+public class StrUtil {
+   /**
+    * 功能描述:替换字符串，将字符串中的固定字符的替换
+    * @Param: [regex 匹配正则, msg 第一个是msg,后面就是替换的str]
+    * @Return: java.lang.String
+    * @Author: Xu yifan
+    * @Date: 2019/8/22 9:38
+    */
+    public static String getMsg(String regex,Object... msg){
+        StringBuilder str=new StringBuilder();
+        int index=1;
+        String msginfo=" "+(String) msg[0]+" ";
+        String[] strArray=null;
+        if (StringUtils.isNotEmpty(msginfo)){
+            strArray = msginfo.split(regex);
+        }
+
+        for (String msgStr : strArray) {
+           str.append(msgStr);
+           if (msg.length-1>index){
+               str.append(msg[index]);
+           }
+            index++;
+        }
+        String result=str.toString().replaceFirst(" ","");
+        return result.substring(0,result.length()-1);
+    }
+}
